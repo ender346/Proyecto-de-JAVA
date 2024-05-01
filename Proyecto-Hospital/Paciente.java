403df24b5aaa4c2e5f590a45e4beea24195ab990
Emanuel Hernández Santiago <107447125+ender346@users.noreply.github.com>
2024-04-30 22:59:05 -0600

    Update Paciente.java

--- Proyecto-Hospital/Paciente.java
+++ Proyecto-Hospital/Paciente.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.gestionclinica;
-
-/**
- *
- * @author 3nder
- */
 public class Paciente {
     private String nombre;
+    private String apellidoPaterno; // Nuevo atributo
+    private String apellidoMaterno; // Nuevo atributo
+    private String sexo;
     private int edad;
     private String numeroSeguridadSocial;
 
-    public Paciente(String nombre, int edad, String numeroSeguridadSocial) {
+    public Paciente(String nombre, String apellidoPaterno, String apellidoMaterno, String sexo, int edad, String numeroSeguridadSocial) {
         this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.sexo = sexo;
         this.edad = edad;
         this.numeroSeguridadSocial = numeroSeguridadSocial;
     }
@@ -23,11 +19,31 @@ public class Paciente {
         return nombre;
     }
 
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
     public int getEdad() {
         return edad;
     }
 
     public String getNumeroSeguridadSocial() {
         return numeroSeguridadSocial;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellidoPaterno + " " + apellidoMaterno;
     }
 }
